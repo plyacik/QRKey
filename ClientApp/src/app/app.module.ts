6ed4f9a058279273835a6c0c1f6b74b70a534510
Юрчик Пляцик <plyacik@debian.plyacik>
2020-05-16 00:23:42 +0300

    add table material

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { QRCodeModule } from 'angular2-qrcode';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -15,6 +18,7 @@ import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
 import { AdminComponent } from './admin/admin.component';
 import { QrkeyComponent } from './admin/qrkey/qrkey.component';
+import { InviteQkkeyComponent } from './admin/invite-qkkey/invite-qkkey.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +28,8 @@ import { QrkeyComponent } from './admin/qrkey/qrkey.component';
     CounterComponent,
     FetchDataComponent,
     AdminComponent,
-    QrkeyComponent
+    QrkeyComponent,
+    InviteQkkeyComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -32,6 +37,9 @@ import { QrkeyComponent } from './admin/qrkey/qrkey.component';
     FormsModule,
     ApiAuthorizationModule,
     QRCodeModule,
+    MatSortModule,
+    MatTableModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
@@ -39,6 +47,7 @@ import { QrkeyComponent } from './admin/qrkey/qrkey.component';
       {
         path: 'admin', component: AdminComponent, children: [
           { path: 'qrkey', component: QrkeyComponent, canActivate: [AuthorizeGuard] },
+          { path: 'invite-qrkey', component: InviteQkkeyComponent, canActivate: [AuthorizeGuard] },
         ]
       },
     ])
